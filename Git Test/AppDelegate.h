91c20e0aa448e17f3b9c4957784878b0b57776df
developer1989 <rafael.rodriguez89@icloud.com>
2017-08-11 06:56:43 -0500

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Rafael Rodriguez on 8/9/17.
+//  Copyright © 2017 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  Git Test
+//
+//  Created by Rafael Rodriguez on 8/9/17.
+//  Copyright © 2017 Personal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Hola Mundo");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
